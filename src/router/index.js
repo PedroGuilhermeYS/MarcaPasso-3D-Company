d22108cd1af67ae1592895acd909fa9ebe052b76
PedroGuilhermeYS <pedro210905@gmail.com>
2025-09-27 16:01:57 -0300

    algumas rotas adicionadas

--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,32 @@
+import ComprarView from '@/views/ComprarView.vue'
+import HomeView from '@/views/HomeView.vue'
+import LoginView from '@/views/LoginView.vue'
 import { createRouter, createWebHistory } from 'vue-router'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [],
+  routes: [
+    { 
+      path: "/",
+      name: "Home",
+      component: HomeView
+     },
+     { 
+      path: "/Login",
+      name: "Login",
+      component: LoginView
+     },
+     { 
+      path: "/Comprar",
+      name: "Comprar",
+      component: ComprarView
+     },
+     { 
+      path: "/Carrinho",
+      name: "Carrinho",
+      component: CarrinhoView
+     },
+    ],
 })
 
 export default router

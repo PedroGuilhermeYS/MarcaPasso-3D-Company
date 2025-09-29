4f8c55613a807c8416b530fd3b71d37c85e9d0ba
PedroGuilhermeYS <pedro210905@gmail.com>
2025-09-29 20:52:26 -0300

    style: adicionado o CSS em listagem de produtos, filtro lateral e boletim informativo

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -5,7 +5,7 @@
         <div class="container">
 
             <div class="filter">
-                <h3 style="margin-left: 10px;"># Filtre sua busca</h3>
+                <h3 style="margin-left: 10px;" class="MS-Reference"># Filtre sua busca</h3>
                 <label for="categorias">
                     <select name="categorias" id="categoria" class="select-filter">
                         <option value="" selected disabled>Categorias</option>
@@ -34,9 +34,9 @@
             </div>
             
             <div class="all-products">
-                <h2>Todos os produtos</h2>
+                <h2 class="MS-Reference">Todos os produtos</h2>
                 <label for="categorias">
-                    <h3>Ordenar por:
+                    <h3 class="MS-Reference">Ordenar por:
                         <select name="relevancia" id="relevancia" class="relevance-filter">
                             <option value="" selected disabled>Relevância</option>
                             <option value="1">Opção 1</option>
@@ -59,3 +59,109 @@
         </div>
     </body>
 </template>
+
+<style scoped>
+
+    body{
+        width: 1400px;
+        margin: 0 auto;
+        font-family: 'Open Sans';
+        font-weight: 300;
+    }
+    .container{
+        display: flex;
+        align-items: flex-start;
+    }
+    .MS-Reference{
+       font-family: "MS Reference Sans Serif", sans-serif;
+       font-weight: 200;
+    }
+    .all-products{
+        width: 78%;
+        margin: 0 auto;
+        border-radius: 20px;
+        border: 2px solid #0185FA;
+    }
+    .all-products h3{
+        margin-top: 5px;
+        margin-left: 40px;
+        font-size: .8rem;
+    }
+    .all-products h2{
+        margin-top: 10px;
+        margin-left: 50px;
+        margin-bottom: 0px;
+    }
+    .relevance-filter{
+        padding: 5px;
+        border-radius: 20px;
+        width: 140px;
+        font-size: .8rem;
+        background-color: #D2D2D2;
+        border: none;
+    }
+    .filter{
+        width: 250px;
+        height: 300px;
+        border-radius: 20px;
+        border: 2px solid #0185FA;
+    }
+    .filter h3{
+        margin-top: 20px;
+        margin-bottom: 10px;
+    }
+    
+    .select-filter{
+        width: 230px;
+        padding: 5px;
+        border-radius: 20px;
+        border-color: #0185FA;
+        margin-left: 10px;
+        margin-bottom: 5px;
+    }
+    .button-filter{
+        width: 130px;
+        padding: 5px;
+        border-radius: 20px;
+        color: white;
+        background-color: #0185FA;
+        border-color: transparent;
+        margin-top: 5px;
+        margin-left: 10px;
+    }
+    .information{
+        box-sizing: border-box;
+        padding: 10px;
+        margin-top: 30px;
+        background-color: #DDDDDD;
+        text-align: center;
+    }
+    .information p{
+        margin: 0px;
+        margin-bottom: 20px;
+        line-height: 15px;
+    }
+    .information h3{
+        margin: 0px;
+    }
+    input{
+        border: none;
+        border-radius: 3px;
+        padding: 5px;
+        margin-left: 10px;
+        width: 10rem;
+    }
+    input::placeholder{
+        color: #D2D2D2;
+    }
+    .button-register{
+        width: 130px;
+        padding: 3px;
+        border-radius: 3px;
+        border-color: transparent;
+        color: white;
+        background-color: #0185FA;
+        margin-left: 10px;
+    }
+    
+</style>

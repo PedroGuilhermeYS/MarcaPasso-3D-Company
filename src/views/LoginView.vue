f00f18df0aed990977c910f060a03751f34ca660
PedroGuilhermeYS <pedro210905@gmail.com>
2025-09-30 23:15:02 -0300

    feat: Adicionado html da tela de login

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,3 +1,22 @@
+<script setup>
+</script>
+
 <template>
-    <p></p>
+    <main>
+        <div class="container">
+            <div class="yes-registered">
+                <h2>Já sou cadastrado</h2>
+                <p>Para se autenticar, informe seu e-mail ou CPF/CNPJ</p>
+                <button class="button-login">ENTRAR</button>
+            </div>
+
+            <div class="Essa-barra-que-é-gostar-de-você"></div>
+
+            <div class="not-registered">
+                <h2>Ainda não tenho cadastro</h2>
+                <p>O cadastro em nossa loja é simples e rápido</p>
+                <button class="button-registed">ENTRAR</button>
+            </div>
+        </div>
+    </main>
 </template>

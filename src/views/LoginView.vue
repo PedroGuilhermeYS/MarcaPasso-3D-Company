a1975419b8fcbbd19f97d130840c9a39748dda12
PedroGuilhermeYS <pedro210905@gmail.com>
2025-09-30 23:22:44 -0300

    style: adicionado o CSS na tela de login, botões e texto

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -20,3 +20,62 @@
         </div>
     </main>
 </template>
+
+<style scoped>
+    main{
+        width: 1400px;
+        margin: 0 auto;
+        font-family: 'Open Sans';
+        font-weight: 300;
+    }
+    .container{
+        width: 100%;
+        display: flex;
+        gap: 10px;
+        justify-content: space-evenly;
+        border: 2px solid #0185FA;
+        border-radius: 20px;
+        box-sizing: border-box;
+        padding: 10rem 4rem;
+        margin-top: 3rem;
+        margin-bottom: 3rem;
+    }
+    .yes-registered, .not-registered{
+        width: 20rem;
+        margin: 0 auto;
+        margin-top: 5rem;
+        margin-bottom: 5rem;
+    }
+    .yes-registered {
+        width: 23rem;
+    }
+    .Essa-barra-que-é-gostar-de-você{
+        border: 2px solid #0185FA;
+        height: 1;
+    }
+    .button-login, .button-registed{
+        width: 12rem;
+        padding: 10px;
+        border-radius: 6px;
+        border-color: transparent;
+        color: white;
+        background-color: #0185FA;
+        
+    }
+    .button-registed{
+        color: #0185FA;
+        background-color: #ffffff;
+        border: 2px solid #0185FA;
+        font-weight: bold;
+    }
+    h2{
+        margin-bottom: 1px;
+        font-weight: bolder;
+    }
+    p{
+        margin-top: 0px;
+        margin-bottom: 1.5rem;
+        line-height: 15px;
+        
+    }
+</style>
